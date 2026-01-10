36740a03be553cd31d6d27eed0f089eb45958d21
fliiiix <hi@l33t.name>
2026-01-10 16:16:31 +0100

    wip

---
+++ pkgs/by-name/ri/ripe-atlas-software-probe/package.nix
@@ -0,0 +1,56 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  openssl,
+  pkg-config,
+  withUser ? "ripe-atlas",
+  withGroup ? "ripe-atlas",
+  withMeasurementUser ? "ripe-atlas-measurement",
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "ripe-atlas-software-probe";
+  version = "5120";
+
+  src = fetchFromGitHub {
+    owner = "ripe-ncc";
+    repo = finalAttrs.pname;
+    tag = finalAttrs.version;
+    hash = "sha256-rjhLLeUj6US76/joRVBmYeqKsPVE5KzZGdE4eEilEKI";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+  ];
+
+  buildInputs = [ openssl ];
+
+  env.NIX_CFLAGS_COMPILE = toString [
+    "-Wno-error=format-security"
+  ];
+
+  configureFlags = [
+    "--enable-systemd"
+    "--disable-chown"
+    "--disable-setcap-install"
+    "--runstatedir=/run"
+    "--with-user=${withUser}"
+    "--with-group=${withGroup}"
+    "--with-measurement-user=${withMeasurementUser}"
+  ];
+
+  preBuild = ''
+    sed -i '/install-exec-local:/,/^$/d' Makefile
+  '';
+
+  meta = {
+    description = "Software probes for the RIPE ATLAS project.";
+    homepage = "https://github.com/RIPE-NCC/ripe-atlas-software-probe";
+    changelog = "https://github.com/RIPE-NCC/ripe-atlas-software-probe/blob/v${finalAttrs.version}/CHANGES.rst";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ raitobezarius ];
+  };
+})
